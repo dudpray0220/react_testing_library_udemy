74fa48f50ae81d1cc6753668d707e59b2a4278b8
YoungHyun Bae <qodudgus0220@naver.com>
2023-10-09 22:06:36 +0900

    Test and code button click

---
+++ color-button/src/App.test.js
@@ -0,0 +1,31 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import App from './App';
+
+test('button has correct initial color, and updates when clicked', () => {
+  render(<App />);
+
+  // find the element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+});
+
+test('button turns blue when clicked', () => {
+  render(<App />);
+
+  // find the element with a role of button and text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+
+  // expect the background color to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background color to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton).toHaveTextContent('Change to red');
+});
